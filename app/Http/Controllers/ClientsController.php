d06475a640928caf90ae88d71fc5dd70a67af10e
Victor Costa <victor.costa.ti21@gmail.com>
2024-08-27 13:05:39 -0300

    init

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClientsController extends Controller
+{
+    public function index(Request $request)
+    {
+        return true;
+    }
+
+    public function store(Request $request)
+    {
+        return true;
+    }
+
+    public function update(Request $request)
+    {
+        return true;
+    }
+
+    public function delete(Request $request)
+    {
+        return true;
+    }
+}
